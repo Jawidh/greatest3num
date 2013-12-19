b9cff0f2eef325883edef688b2d1c8b9193ac3e9
BSA Univ6 <bsauniv6@BSAs-iMac-6.local>
2013-12-19 16:38:19 +0530

    Folder added

---
+++ greatest3/main.m
@@ -0,0 +1,24 @@
+//
+//  main.m
+//  greatest3
+//
+//  Created by BSA Univ6 on 19/12/13.
+//  Copyright (c) 2013 BSA. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "greatest3.h"
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Greatest of 3 numbers");
+        greatest3 *obj=[[greatest3 alloc]init];
+        [obj great];
+    }
+    return 0;
+}
+
